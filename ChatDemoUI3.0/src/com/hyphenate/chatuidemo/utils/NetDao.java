40abe9a3244cf74cf4b9b8ae5f919cef67f9fa42
liuran <1227228155@qq.com>
2016-11-10 21:41:27 +0800

    添加上传群组图标功能，创建群组到服务器，

--- ChatDemoUI3.0/src/com/hyphenate/chatuidemo/utils/NetDao.java
+++ ChatDemoUI3.0/src/com/hyphenate/chatuidemo/utils/NetDao.java
@@ -3,6 +3,7 @@ package com.hyphenate.chatuidemo.utils;
 import android.content.Context;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chatuidemo.I;
 import com.hyphenate.chatuidemo.bean.Result;
 import com.hyphenate.easeui.domain.Group;
@@ -103,24 +104,36 @@ public class NetDao {
                 .execute(listener);
     }
 
-    public static void creatGroup(Context context,String hxid,String groupname,String description,String owner,String ispublic,String invites
-                                ,File file,OkHttpUtils.OnCompleteListener<Group> listener ){
-        OkHttpUtils<Group> utils = new OkHttpUtils<>(context);
+    public static void creatGroup(Context context, EMGroup emGroup, File file, OkHttpUtils.OnCompleteListener<String> listener ){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
-                .addParam(I.Group.HX_ID,hxid)
-                .addParam(I.Group.NAME,groupname)
-                .addParam(I.Group.DESCRIPTION,description)
-                .addParam(I.Group.OWNER,owner)
-                .addParam(I.Group.IS_PUBLIC,ispublic)
-                .addParam(I.Group.ALLOW_INVITES,invites)
-                .targetClass(Group.class)
+                .addParam(I.Group.HX_ID,emGroup.getGroupId())
+                .addParam(I.Group.NAME,emGroup.getGroupName())
+                .addParam(I.Group.DESCRIPTION,emGroup.getDescription())
+                .addParam(I.Group.OWNER,emGroup.getOwner())
+                .addParam(I.Group.IS_PUBLIC,String.valueOf(emGroup.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES,String.valueOf(emGroup.isAllowInvites()))
+                .targetClass(String.class)
                 .addFile2(file)
+                .post()
+                .execute(listener);
+    }
+
+    public static void creatGroup(Context context, EMGroup emGroup,  OkHttpUtils.OnCompleteListener<String> listener ){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
+                .addParam(I.Group.HX_ID,emGroup.getGroupId())
+                .addParam(I.Group.NAME,emGroup.getGroupName())
+                .addParam(I.Group.DESCRIPTION,emGroup.getDescription())
+                .addParam(I.Group.OWNER,emGroup.getOwner())
+                .addParam(I.Group.IS_PUBLIC,String.valueOf(emGroup.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES,String.valueOf(emGroup.isAllowInvites()))
+                .targetClass(String.class)
                 .post()
                 .execute(listener);
 
 
     }
-
 
 
 }
