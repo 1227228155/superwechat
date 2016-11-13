2e86beaddcf5edf38e2d59a7f3817d7b574d28ae
liuran <1227228155@qq.com>
2016-11-13 15:59:48 +0800

    修改群组相关的标题为黑色

--- ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/ContactListFragment.java
+++ ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/ContactListFragment.java
@@ -145,7 +145,7 @@ public class ContactListFragment extends EaseContactListFragment {
                 if (user != null) {
                     String username = user.getMUserName();
                     // demo中直接进入聊天页面，实际一般是进入用户详情页
-                    MFGT.gotoFriend(getActivity(),SuperWeChatHelper.getInstance().getAppContactList().get(username));
+                    MFGT.gotoFriend(getActivity(),username);
 
                 }
             }
